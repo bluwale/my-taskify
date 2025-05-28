7cddf56a8980bb4aea0366a5ede844b22d800327
bluwale <saad.sanaullah04@gmail.com>
2025-05-28 18:47:05 -0400

    FirstCommit

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Todo } from '../model';
+import {AiFillDelete, AiFillEdit} from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import './styles.css'
+
+
+type Props = {
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+
+const SingleTodo = ({todo,todos, setTodos}: Props) => {
+  
+  /*function checks if todo is done or not and toggles the isDone property */
+    const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  
+    return (
+    <form className="todos_single">
+        {
+            todo.isDone ? (
+                <s className="todos_single--text">{todo.todo}</s>
+
+            ):(
+                <span className="todos_single--text">{todo.todo}</span>
+
+            )
+        }
+
+            
+        <div>
+            <span className='icon'> 
+                <AiFillEdit/>
+            </span>
+            <span className='icon'>
+                <AiFillDelete/>
+            </span>
+            <span className='icon'> 
+                <MdDone/>
+            </span>
+        </div>
+
+    </form>
+  )
+}
+
+export default SingleTodo
