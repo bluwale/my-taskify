301ef423be84ee157099347d6c3fd122cb40816e
bluwale <saad.sanaullah04@gmail.com>
2025-06-01 22:26:28 -0400

    ThirdCommit,

---
+++ public/electron.js
@@ -0,0 +1,47 @@
+// public/electron.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Simple development check without electron-is-dev
+const isDev = process.env.NODE_ENV === 'development' || process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath);
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    if (mainWindow.webContents) {
+      mainWindow.webContents.openDevTools();
+    }
+  } else {
+    // Load the built React app
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
